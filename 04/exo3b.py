6d16986952771975a277e6a8f850a44a3fe7986f
Eric Dejonckheere <eric@aya.io>
2014-07-04 17:13:00 +0200

    03b.py

---
+++ 04/exo3b.py
@@ -0,0 +1,57 @@
+# _*_ encoding: utf-8 _*_
+import sys
+import json
+import urllib
+
+class NasaExo():
+
+    def __init__(self, params):
+        self.year = params[1]
+        self.api_base = 'http://exoapi.com/api/skyhook/'
+
+    def get_planets(self):
+        return json.loads(self.download(self.make_url_planets_year()))['response']['results']
+
+    def download(self, url):
+        return urllib.urlopen(url).read()
+
+    def get_names(self, planet_list):
+        return [planet['name'] for planet in planet_list]
+
+    def print_list(self, my_list):
+        for obj in my_list:
+            print obj
+
+    def make_url_planets_year(self):
+        return self.api_base + 'planets/search?disc_year=' + self.year
+
+    def print_names(self):
+        return self.print_list(self.get_names(self.get_planets()))
+
+    def make_details(self):
+        result = []
+        for obj in self.get_planets():
+            x = {
+                'name': obj['name'],
+                'class': obj['mass_class'],
+                'atmosphere': obj['atmosphere_class'],
+                'composition': obj['composition_class'],
+                'mass': obj['mass'],
+                'gravity': obj['gravity'],
+                'size': obj['appar_size'],
+                'star': obj['star']['name'],
+                'constellation': obj['star']['constellation']
+            }
+            result.append(x)
+        return result
+
+    def print_details(self):
+        for planet_details in self.make_details():
+            print "Name".ljust(16), ":", planet_details['name']
+            del planet_details['name']
+            for key,value in planet_details.iteritems():
+                print key.capitalize().ljust(16), ":", str(value).capitalize()
+            print ""
+
+exo = NasaExo(sys.argv)
+exo.print_details()
